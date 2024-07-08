5c3d6a88f33b5336ab6481e74e10119f9e34c029
Yousuf <firedragonironfist998@gmail.com>
2024-07-08 20:34:50 +0530

    Ninth Commit

--- server.js
+++ server.js
@@ -16,6 +16,41 @@ app.get('/form', (req, res) => {
   res.sendFile(__dirname + '/client/form.html');
 });
 
+app.post('/submit', (req,res) => {
+  const data = req.body;
+  var email = data[0].email;
+  var firstName = data[0].firstName;
+  var lastName = data[0].lastName;
+  var message = data[0].message;
+  var tel = data[0].tel;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    auth: {
+      user: "info@sauber-weg.de",
+      pass: "cdfydzpdtfjkxjqd"
+    },
+  });
+  
+  let emailText = '';
+  emailText = `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nMessage: ${message}\nTelephone: ${tel}`;
+
+  const mailOptions = {
+    from: "info@sauber-weg.de",
+    to: email,
+    subject: "Thank you for your submission!",
+    text: emailText,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ success: false, error: error.toString() });
+    }
+    res.status(200).json({ success: true, info });
+  });
+});
+
 app.post("/form/submit", (req, res) => {
   const data = req.body;
   var Jetzt = data[0].Jetzt;
